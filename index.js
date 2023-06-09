191c66d90da6fa10bf4588de2b91da9eca8c7005
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-06-09 17:32:01 +0200

    JS - daily and daily previous

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 fetch('./data.json').then(response => response.json()).then(
-    data => { console.log(data);
-        let currentHrsH1s = document.getElementsByClassName("currentHrs");
+    data => {;
 
-        let dailyCurrentHrs = data[0].timeframes.daily.current;
-        currentHrsH1s[0].innerText = dailyCurrentHrs + 'hrs';
+        let currentHrsH1s = document.getElementsByClassName("currentHrs");
+        let previousHrsPs = document.getElementsByClassName("previousHrs");
+        
+
+        // Daily time frame
+        for (let i = 0; i < data.length; i++) {
+
+        let dailyCurrentHrs = data[i].timeframes.daily.current;
+
+        currentHrsH1s[i].innerText = dailyCurrentHrs + 'hrs';
+
+    }
+
+        // Previous daily
+
+        for (i = 0; i < data.length; i++) {
+
+        let dailyPreviousHrsPs = data[i].timeframes.daily.previous;
+        previousHrsPs[i].innerText = 'Previous ' + ' - ' + dailyPreviousHrsPs + 'hrs'
+        
+    }
+
+
+
+
+
 
 
 
