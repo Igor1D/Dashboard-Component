e0cfdefb44f78313fce8e8436cd16a1c23c40a7b
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-06-09 18:18:16 +0200

    "js daily part done"

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 fetch('./data.json').then(response => response.json()).then(
     data => {;
 
+        let dailyBtn = document.getElementsByClassName("dailyBtn");
+        let weeklyBtn = document.getElementsByClassName("weeklyBtn");
+        let monthlyBtn = document.getElementsByClassName("monthlyBtn");
         let currentHrsH1s = document.getElementsByClassName("currentHrs");
         let previousHrsPs = document.getElementsByClassName("previousHrs");
         
@@ -22,6 +25,50 @@ fetch('./data.json').then(response => response.json()).then(
         previousHrsPs[i].innerText = 'Previous ' + ' - ' + dailyPreviousHrsPs + 'hrs'
         
     }
+
+    
+
+
+
+
+
+        // // Weekly time frame
+        
+        // for (let i = 0; i < data.length; i++) {
+
+        // let weeklyCurrentHrs = data[i].timeframes.weekly.current;
+        
+    
+        // currentHrsH1s[i].innerText = weeklyCurrentHrs + 'hrs';
+        // }
+
+        // // Previous weekly
+
+        // for (i = 0; i < data.length; i++) {
+
+        // let weeklyPreviousHrsPs = data[i].timeframes.weekly.previous;
+        // previousHrsPs[i].innerText = 'Previous ' + ' - ' + weeklyPreviousHrsPs + 'hrs'
+
+        // }
+
+        // // Monthly time frame
+
+        // for (let i = 0; i < data.length; i++) {
+
+        // let monthlyCurrentHrs = data[i].timeframes.monthly.current;
+            
+        
+        // currentHrsH1s[i].innerText = monthlyCurrentHrs + 'hrs';
+        // }
+    
+        // // Previous monthly
+    
+        // for (i = 0; i < data.length; i++) {
+    
+        // let monthlyPreviousHrsPs = data[i].timeframes.monthly.previous;
+        // previousHrsPs[i].innerText = 'Previous ' + ' - ' + monthlyPreviousHrsPs + 'hrs'
+    
+        // }
 
 
 
