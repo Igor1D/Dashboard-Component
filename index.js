6076db8978ad15d69e2b3a4b63d157e705bf9ab8
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-06-12 21:55:47 +0200

    worked on the JS part - stucked at the buttons style while clicked

--- index.js
+++ index.js
@@ -1,97 +1,107 @@
 fetch('./data.json').then(response => response.json()).then(
-    data => {;
+    data => {
+        ;
 
-        let dailyBtn = document.getElementsByClassName("dailyBtn");
-        let weeklyBtn = document.getElementsByClassName("weeklyBtn");
-        let monthlyBtn = document.getElementsByClassName("monthlyBtn");
+        let dailyBtn = document.getElementById("dailyBtn");
+        let weeklyBtn = document.getElementById("weeklyBtn");
+        let monthlyBtn = document.getElementById("monthlyBtn");
         let currentHrsH1s = document.getElementsByClassName("currentHrs");
         let previousHrsPs = document.getElementsByClassName("previousHrs");
+        let allTimeFramesBtns = document.querySelectorAll(".timeFramesBtn");
+        console.log(allTimeFramesBtns);
+
         
+        // Check whether a button is selected
+        let btnIsClicked = false;
+        let selectedBtn = false;
+        for (let i = 0; i < allTimeFramesBtns.length; i++) {
 
-        // Daily time frame
-        for (let i = 0; i < data.length; i++) {
+            allTimeFramesBtns[i].addEventListener('click', () => {
+                if (selectedBtn == true) {
+                    selectedBtn.style.color = " ";
+                }
+                selectedBtn = allTimeFramesBtns[i];
+                selectedBtn.style.color = "white"
 
-        let dailyCurrentHrs = data[i].timeframes.daily.current;
+                console.log(selectedBtn);
 
-        currentHrsH1s[i].innerText = dailyCurrentHrs + 'hrs';
-
-    }
-
-        // Previous daily
-
-        for (i = 0; i < data.length; i++) {
-
-        let dailyPreviousHrsPs = data[i].timeframes.daily.previous;
-        previousHrsPs[i].innerText = 'Previous ' + ' - ' + dailyPreviousHrsPs + 'hrs'
-        
-    }
-
-    
-
-
-
-
-
-        // // Weekly time frame
-        
-        // for (let i = 0; i < data.length; i++) {
-
-        // let weeklyCurrentHrs = data[i].timeframes.weekly.current;
-        
-    
-        // currentHrsH1s[i].innerText = weeklyCurrentHrs + 'hrs';
-        // }
-
-        // // Previous weekly
-
-        // for (i = 0; i < data.length; i++) {
-
-        // let weeklyPreviousHrsPs = data[i].timeframes.weekly.previous;
-        // previousHrsPs[i].innerText = 'Previous ' + ' - ' + weeklyPreviousHrsPs + 'hrs'
-
-        // }
-
-        // // Monthly time frame
-
-        // for (let i = 0; i < data.length; i++) {
-
-        // let monthlyCurrentHrs = data[i].timeframes.monthly.current;
+                // btnIsClicked = true;
+                // if (btnIsClicked) {
+                //     allTimeFramesBtns[i].style.color = "white";
+                // } 
+            })
             
+        }
+       
         
-        // currentHrsH1s[i].innerText = monthlyCurrentHrs + 'hrs';
-        // }
-    
-        // // Previous monthly
-    
-        // for (i = 0; i < data.length; i++) {
-    
-        // let monthlyPreviousHrsPs = data[i].timeframes.monthly.previous;
-        // previousHrsPs[i].innerText = 'Previous ' + ' - ' + monthlyPreviousHrsPs + 'hrs'
-    
-        // }
+        
+
+        dailyBtn.addEventListener('click', () => {
+            // Daily time frame
+            for (let i = 0; i < data.length; i++) {
+
+                let dailyCurrentHrs = data[i].timeframes.daily.current;
+
+                currentHrsH1s[i].innerText = dailyCurrentHrs + 'hrs';
+
+            }
+
+            // Previous daily
+
+            for (i = 0; i < data.length; i++) {
+
+                let dailyPreviousHrsPs = data[i].timeframes.daily.previous;
+                previousHrsPs[i].innerText = 'Previous ' + ' - ' + dailyPreviousHrsPs + 'hrs'
+
+            }
+            
+            
+
+        })
+
+        weeklyBtn.addEventListener('click', () => {
+            // Weekly time frame
+            for (let i = 0; i < data.length; i++) {
+
+                let weeklyCurrentHrs = data[i].timeframes.weekly.current;
+
+                currentHrsH1s[i].innerText = weeklyCurrentHrs + 'hrs';
+
+            }
+
+            // Previous Weekly
+
+            for (i = 0; i < data.length; i++) {
+
+                let weeklyPreviousHrsPs = data[i].timeframes.weekly.previous;
+                previousHrsPs[i].innerText = 'Previous ' + ' - ' + weeklyPreviousHrsPs + 'hrs'
+
+            }
+
+            
+
+        })
 
 
+        monthlyBtn.addEventListener('click', () => {
+            // Monthly time frame
+            for (let i = 0; i < data.length; i++) {
 
+                let monthlyCurrentHrs = data[i].timeframes.monthly.current;
 
+                currentHrsH1s[i].innerText = monthlyCurrentHrs + 'hrs';
 
+            }
 
+            // Previous Monthly
 
+            for (i = 0; i < data.length; i++) {
 
+                let monthlyPreviousHrsPs = data[i].timeframes.monthly.previous;
+                previousHrsPs[i].innerText = 'Previous ' + ' - ' + monthlyPreviousHrsPs + 'hrs'
 
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        })
 
     })
